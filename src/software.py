02667c9c7bc00db62ab10d21ce5408594968064f
Kalos Lazo <lzm.kalos@gmail.com>
2023-03-01 16:11:57 -0500

    first commit

---
+++ src/software.py
@@ -0,0 +1,46 @@
+soft_list=[
+    # TERMINAL UTILITIES --------------------------------------------------
+    'bash-completion',       # tab completion for Bash
+    'curl',                  # remote content retrieval
+    'feh',                   # terminal-based image viewer/manipulator
+    'unrar',                 # RAR compression program
+    'unzip',                 # zip compression program
+    'wget',                  # remote content retrieval
+    'zip',                   # zip compression program
+    'zsh',                   # zSH shell
+    'zsh-completions',       # tab completion for ZSH
+    'rofi',                  # menu for apps
+    'ranger',                # terminal file manager
+
+    # DEVELOPMENT ---------------------------------------------------------
+    'atom',                  # text editor
+    'apache',                # apache web server
+    'clang',                 # C Lang compiler
+    'cmake',                 # cross-platform open-source make system
+    'electron',              # cross-platform development using Javascript
+    'git',                   # version control system
+    'gcc',                   # C/C++ compiler
+    'glibc',                 # C libraries
+    'mariadb',               # drop-in replacement for MySQL
+    'nodejs',                # javascript runtime environment
+    'npm',                   # node package manager
+    'php',                   # web application scripting language
+    'php-apache',            # apache PHP driver
+    'postfix',               # SMTP mail server
+    'python',                # scripting language
+    'qtcreator',             # c++ cross platform IDE
+    'qt5-examples',          # project demos for Qt
+    'yarn',                  # dependency management (Hyper needs this)
+    'python-pip',            # pip modules manager
+    'ipython',               # alt console admin for python
+    'xournalpp',             # digital notes and drawing software 
+    'gdb',                   # allow us run on vscode g++ compiler                   
+    'qtile',                 # default windows manager
+
+    # GRAPHICS AND DESIGN -------------------------------------------------
+    'gcolor2',               # colorpicker
+    'gimp',                  # GNU Image Manipulation Program
+    'inkscape',              # vector image creation app
+    'krita',                 # image tool editor
+    'libreoffice-fresh',     # libre office with extra features
+];
---
+++ todo.md
@@ -0,0 +1,24 @@
+[AFTER]
+- git
+- python3
+- yay (pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si)
+
+[DEVELOPMENT]
+- vscode
+- neovim
+- alacritty
+- qtile
+- zsh
+- starship
+- python-pip
+- caskaydiacove NF
+
+[SOFTWARE]
+- firefox
+- telegram
+- rofi
+- zathura
+
+[TOOLS]
+- fontbase
+- macchina
